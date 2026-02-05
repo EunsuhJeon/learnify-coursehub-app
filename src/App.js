af974df172338f06d8fe54d396edea2388c932df
Jade <lilyjun98@gmail.com>
2026-02-05 14:50:48 -0800

    Setup routing structure and main layout

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="login" element={<Login />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
