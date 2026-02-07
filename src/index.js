0693622f4b50a655eadf5cbe7b0f11a3de00a86d
Isabella Marcucci <bellamarcucci@gmail.com>
2026-02-06 21:49:05 -0800

    Revert "Header/Navbar bootstrap & CSS"

--- src/index.js
+++ src/index.js
@@ -2,20 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./index.css";
 
-
-
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </BrowserRouter>
+  <App />
 );
--- src/pages/Courses.js
+++
@@ -1,7 +0,0 @@
-export default function Courses() {
-    return (
-        <div>
-            <h1>Courses Page</h1>
-        </div>
-    )
-}
