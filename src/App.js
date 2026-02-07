8ac14a0179a0539b53cecd1067cd824132d6d5c9
bella-marcucci <bellamarcucci@gmail.com>
2026-02-06 21:39:40 -0800

    header bug fixed

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Login from "./pages/Login";
 import "./App.css";
-import { AppHeader } from "./components/AppHeader";
 
 function App() {
   return (
-    <><Routes>
-          <Route path="/" element={<MainLayout />}>
-              <Route path="login" element={<Login />} />
-          </Route>
-      </Routes><>
-              <AppHeader />
-              <main className="container py-4">
-                  <h1 className="h4">App</h1>
-              </main>
-          </></>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="login" element={<Login />} />
+      </Route>
+    </Routes>
   );
 }
-
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-export default function Header() {
-  return (
-    <nav>
-      <Link to="/login">Login</Link>
-    </nav>
-  );
-}
+// export default function Header() {
+//   return (
+//     <nav>
+//       <Link to="/login">Login</Link>
+//     </nav>
+//   );
+// }
