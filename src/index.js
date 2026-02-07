b906cf9f258f1a0292c9f80f96f73b8cebab0f36
Santiagoo <santiagominotta2913@hotmail.com>
2026-02-06 23:28:26 -0800

    add mock login API integration with auth context

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+//import { AuthProvider } from './contexts/AuthContext';
 import { AuthProvider } from './contexts/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
